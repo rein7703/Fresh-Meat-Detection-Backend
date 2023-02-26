bd9a3339d65e952ab628a8323671c87507c89723
hasanchamdany <hasanchamdany@gmail.com>
2023-02-26 17:48:30 +0700

    fix(clean): clean code implementation

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from keras.utils import load_img, img_to_array
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
+# from gevent.pywsgi import WSGIServer
 
 print("successfully running")
 
@@ -35,37 +35,8 @@ model = load_model(MODEL_PATH)
 # model._make_predict_function()          # Necessary
 # print('Model loaded. Start serving...')
 
-# You can also use pretrained model from Keras
-# Check https://keras.io/applications/
-#from keras.applications.resnet50 import ResNet50
-#model = ResNet50(weights='imagenet')
-#model.save('')
 print('Model loaded. Check http://127.0.0.1:5000/')
 
-# default prediction section
-# def model_predict(img_path, model):
-#     # img = image.load_img(img_path, target_size=(224, 224))
-#     img = load_img(img_path, target_size=(224, 224))
-
-#     # Preprocessing the image
-#     # x = image.img_to_array(img)
-#     x = img_to_array(img)
-#     # x = np.true_divide(x, 255)
-#     x = np.expand_dims(x, axis=0)
-
-#     # Be careful how your trained model deals with the input
-#     # otherwise, it won't make correct prediction!
-#     x = preprocess_input(x, mode='caffe')
-
-#     preds = model.predict(x)
-#     return preds
-
-# coba coba prediction section
-# Name=[]
-# N=[]
-# reverse_mapping=dict(zip(N,Name)) 
-# def mapper(value):
-#     return reverse_mapping[value]
 
 def model_predict(img_path, model):
     # img = image.load_img(img_path, target_size=(224, 224))
@@ -83,38 +54,9 @@ def model_predict(img_path, model):
 
     preds = model.predict(prediction_image)
     print(preds)
-    # value = np.argmax(preds)
-    # move_name = mapper(value)
-    # print(value)
-    # res = format(move_name)
     return preds
 
 
-# chatGPT code 
-# def model_predict(img_path, model):
-#     # Load and preprocess the image
-#     img = load_img(img_path, target_size=(100, 100))
-#     x = img_to_array(img)
-#     x = preprocess_input(x)
-
-#     # Make prediction
-#     preds = model.predict(x)
-
-#     # Decode the predictions
-#     pred_class = decode_predictions(preds, top=5)[0]
-
-#     # Format the predictions as a string
-#     result = ''
-#     for pred in pred_class:
-#         result += f'{pred[1]}: {pred[2]:.4f}\n'
-
-#     return result
-# end chatGPT code
-
-# @app.route('/', methods=['GET'])
-# def index():
-#     # Main page
-#     return render_template('index.html')
 Name = ['Fresh', 'Spoiled']
 N=[]
 for i in range(len(Name)):
@@ -138,17 +80,6 @@ def upload():
         # Make prediction
         preds = model_predict(file_path, model)
 
-        # solusi chatGPT rada ngawur
-        # pred_array = np.zeros((1, 1000))
-        # # Set the relevant elements of the array to the output of the model
-        # for i, p in enumerate(preds[0]):
-        #     pred_array[0][i] = p
-        # end solusi chatGPT rada ngawur
-        
-        # Process your result for human
-        # pred_class = preds.argmax(axis=-1)            # Simple argmax
-        # pred_class = decode_predictions(preds, top=3)[0]   # ImageNet Decode
-        # result = str(pred_class[0][0][1])               # Convert to string
         value = np.argmax(preds)
         move_name = mapper(value)
         result = "predictions is {}.".format(move_name)
